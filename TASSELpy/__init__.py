ea76951d6bfec3c97594551b3926c2cb1fb34f49
Eli Rodgers-Melnick <er432@cornell.edu>
2014-08-13 23:37:32 -0400

    Finished easy_GLM

--- TASSELpy/__init__.py
+++ TASSELpy/__init__.py
@@ -1,2 +1,2 @@
 from TASSELpy.TASSELbridge import TASSELbridge
-
+from TASSELpy.net.maizegenetics.analysis.association.FixedEffectLMPlugin import easy_GLM
--- TASSELpy/net/maizegenetics/analysis/association/FixedEffectLMPlugin.py
+++ TASSELpy/net/maizegenetics/analysis/association/FixedEffectLMPlugin.py
@@ -1,7 +1,14 @@
 from TASSELpy.net.maizegenetics.plugindef.AbstractPlugin import AbstractPlugin
+from TASSELpy.net.maizegenetics.analysis.data.IntersectionAlignmentPlugin import IntersectionAlignmentPlugin
+from TASSELpy.net.maizegenetics.dna.snp.GenotypeTable import GenotypeTable
 from TASSELpy.utils.helper import make_sig
 from TASSELpy.utils.Overloading import javaConstructorOverload, javaOverload
 from TASSELpy.java.lang.Object import Object
+from TASSELpy.java.util.LinkedList import LinkedList
+from TASSELpy.net.maizegenetics.plugindef.DataSet import DataSet
+from TASSELpy.net.maizegenetics.plugindef.Datum import Datum
+from TASSELpy.net.maizegenetics.trait.Phenotype import Phenotype
+from TASSELpy.net.maizegenetics.util.TableReport import TableReport
 from TASSELpy.java.lang.Boolean import Boolean, metaBoolean
 from TASSELpy.java.lang.Double import metaDouble
 from TASSELpy.java.lang.Integer import metaInteger
@@ -119,6 +126,25 @@ class FixedEffectLMPlugin(AbstractPlugin):
 
 class easy_GLM(FixedEffectLMPlugin):
     """ Subclass of FixedEffectLMPlugin that allows for easy running of GLMs
+
+    Example
+    -------
+
+    >>> from TASSELpy import TASSELbridge
+    >>> TASSELbridge.start()
+    >>> from TASSELpy.data.data_constants import *
+    >>> from TASSELpy.net.maizegenetics.dna.snp.ImportUtils import ImportUtils
+    >>> from TASSELpy.net.maizegenetics.trait.ReadPhenotypeUtils import ReadPhenotypeUtils
+    >>> from TASSELpy.net.maizegenetics.analysis.association.FixedEffectLMPlugin import easy_GLM
+    >>> inputAlign = ImportUtils.readFromHapmap(HAPMAP_FILE)
+    Reading :/Users/eli/Development/TASSELpy/TASSELpy/data/mdp_genotype.hmp.txt  finished
+    >>> traits = ReadPhenotypeUtils.readGenericFile(TRAITS_FILE)
+    >>> pop = ReadPhenotypeUtils.readGenericFile(POP_STRUCTURE_FILE)
+    >>> glm = easy_GLM()
+    >>> glm.addPhenotype(traits,'traits')
+    >>> glm.addMarkers(inputAlign,'markers')
+    >>> glm.addCovariate(pop,'populations')
+    >>> marker_effects, allele_effects = glm.run_glm(phenotypes=('traits',),markers='markers',covariates=('populations',))    
     """
     def __init__(self, outputFile = None, restrictOutput = False, maxP = None,
                  permute = False, numberOfPermutations = 1000, seed = None):
@@ -141,6 +167,11 @@ class easy_GLM(FixedEffectLMPlugin):
         """
         # Call the java constructor
         super(easy_GLM, self).__init__(None, False)
+        # Create dictionaries of Datum_name -> Datum for phenotypes, covariates,
+        # and alignments
+        self.phenotypes = {}
+        self.covariates = {}
+        self.alignments = {}
         # Set the parameters
         if outputFile:
             self.setOutputFile(outputFile)
@@ -151,10 +182,125 @@ class easy_GLM(FixedEffectLMPlugin):
         self.setNumberOfPermutations(numberOfPermutations)
         if seed:
             self.setRandomizer(seed)
-    # TODO: Finish easy_GLM class
+    def addPhenotype(self, pheno, name, comment=None):
+        """ Adds a Phenotype object to the analysis
 
+        Parameters
+        ----------
+        pheno : Phenotype object
+            Phenotype object consisting of 1 or more traits to analyze
+        name : str
+            The name to give to this Phenotype datum
+        comment : str, optional
+            A comment on the Phenotype. If None, will be the same as name
+
+        Raises
+        ------
+        TypeError:
+            if pheno is not a Phenotype or name is not a string
+        """
+        if not isinstance(pheno, Phenotype):
+            raise TypeError("pheno is not a Phenotype object")
+        if not isinstance(name, metaString):
+            raise TypeError("name is not a string")
+        if not comment:
+            comment = str(name)
+        elif not isinstance(comment, metaString):
+            raise TypeError("comment is not a string")
+        self.phenotypes[name] = Datum(name, pheno, comment)
+    def addCovariate(self, covar, name, comment=None):
+        """ Adds a covariate to the analysis
+
+        Parameters
+        ----------
+        covar : Phenotype object
+            Phenotype object consisting of 1 or more covariates
+        name : str
+            The name to give to this covariate datum
+        comment : str, optional
+            A comment on the covariate. If None, will be the same as name
+
+        Raises
+        ------
+        TypeError:
+            if covariate is not a Phenotype or name is not a string
+        """
+        if not isinstance(covar, Phenotype):
+            raise TypeError("covar is not a Phenotype object")
+        if not isinstance(name, metaString):
+            raise TypeError("name is not a string")
+        if not comment:
+            comment = str(name)
+        elif not isinstance(comment, metaString):
+            raise TypeError("comment is not a string")
+        # Ensure all traits are covariates
+        for i in xrange(covar.getNumberOfTraits()):
+            if covar.getTrait(i).getType() != 'covariate':
+                covar.getTrait(i).setType('covariate')
+        self.covariates[name] = Datum(name, covar, comment)
+    def addMarkers(self, markers, name, comment=None):
+        """ Adds markers to the analysis
+
+        Parameters
+        ----------
+        markers : GenotypeTable object
+            Genotype table givng the genetic markers
+        name : str
+            The name to give to this set of markers
+        comment : str, optional
+            A comment on the markers. If None, will be the same as name
+
+        Raises
+        ------
+        TypeError
+            If markers is not a GenotypeTable or name not a string
+        """
+        if not isinstance(markers, GenotypeTable):
+            raise TypeError("markers is not a GenotypeTable object")
+        if not isinstance(name, metaString):
+            raise TypeError("name is not a string")
+        if not comment:
+            comment = str(name)
+        elif not isinstance(comment, metaString):
+            raise TypeError("comment is not a string")
+        self.alignments[name] = Datum(name, markers, comment)
+    def run_glm(self, phenotypes, markers, covariates = None):
+        """ Runs the GLM and returns the results as a TableReport
+
+        Parameters
+        ----------
+        phenotypes : iterable of strings
+            The names of the phenotypes you want to run
+        markers : str
+            The name of the marker set you want to use
+        covariates : iterable of strings, optional
+            The names of the covariates you want to use
+
+        Raises
+        ------
+        KeyError
+            If names not present
+        
+        Returns
+        -------
+        TableReport of marker effects, TableReport of allelic effects
+        """
+        dataList = LinkedList(generic=(Datum,))
+        for pheno in phenotypes:
+            dataList.add(self.phenotypes[pheno])
+        dataList.add(self.alignments[markers])
+        if covariates:
+            for covar in covariates:
+                dataList.add(self.covariates[covar])
+        # Make the dataset, and turn into MarkerPhenotype object
+        ds = DataSet(dataList, None)
+        ds = IntersectionAlignmentPlugin(None, False).performFunction(ds)
+        resultSet = FixedEffectLMPlugin(None, False).performFunction(ds)
+        marker_effects = resultSet.getData(0).getData().castTo(TableReport)
+        allele_effects = resultSet.getData(1).getData().castTo(TableReport)
+        return marker_effects, allele_effects
 """
-    from TASSELpy.TASSELbridge import TASSELbridge
+from TASSELpy.TASSELbridge import TASSELbridge
 TASSELbridge.start()
 from TASSELpy.data.data_constants import *
 from TASSELpy.net.maizegenetics.dna.snp.ImportUtils import ImportUtils
